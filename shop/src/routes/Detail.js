c5583dc166a594bcc00500a7eb4b29a37e98d32a
duhyeonChoi <engus02@loadit.co.kr>
2024-01-10 16:32:13 +0900

    styled 컴포넌트

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,4 +1,17 @@
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+let Box = styled.div`
+  padding : 20px;
+  color : grey
+`;
+let YellowBtn = styled.button`
+background : yellow;
+// background : ${ props => props.bg == 'blue' ? 'white' : 'black' };
+  color : black;
+  padding : 10px;
+`;
+
 
 export default function Detail(props) {
 
@@ -8,13 +21,16 @@ export default function Detail(props) {
   });
 
   return (
+  <>
+    <YellowBtn>버튼</YellowBtn>
+    {/* <YellowBtn bg='orange'>버튼</YellowBtn> */}
     <div className="container">
       <div className="row">
         <div className="col-md-6">
           <img
             src={"https://codingapple1.github.io/shop/shoes"+(parseInt(id)+1)+".jpg"}
             width="100%"
-          /> 
+            /> 
         </div>
         <div className="col-md-6">
           <h4 className="pt-5">{toShow.title}</h4>
@@ -24,5 +40,6 @@ export default function Detail(props) {
         </div>
       </div>
     </div>
+  </>
   );
 }
