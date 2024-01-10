a6e46879cd9f8ec2037fbacc872f09222f040987
duhyeonChoi <engus02@loadit.co.kr>
2024-01-10 17:02:52 +0900

    useEffect 사용법

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,30 +1,47 @@
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
-
-let Box = styled.div`
-  padding : 20px;
-  color : grey
-`;
-let YellowBtn = styled.button`
-background : yellow;
-// background : ${ props => props.bg == 'blue' ? 'white' : 'black' };
-  color : black;
-  padding : 10px;
-`;
-
+import { useEffect, useState } from "react";
 
 export default function Detail(props) {
 
   let { id } = useParams();
-  let toShow = props.shoes.find(function(x){
-    return x.id == id;
-  });
+  let toShow = props.shoes.find( x => x.id == id );
+  let [ count, setCount ] = useState(0);
+  let [ alert, setAlert ] = useState(true);
+
+  useEffect( ()=>{
+    // 랜더링이 다 완료 되고 나서 이 안에 있는 것들이 실행됨
+    // 데이터 가져올 때, 긴 연산이 필요할 때
+    let a = setTimeout( ()=>{ setAlert(false) }, 2000 )    
+    
+    return ()=>{
+      clearTimeout(a)
+    }
+  }, [])
+
+  // useEffect( ()=>{} ) // 랜더링 할 때마다 실행
+  // useEffect( ()=>{}, [] ) // 1회만 실행
+  // useEffect( ()=>{
+  //   return ()=>{
+  //     ~~
+  //   }
+  // }) // return 이전의 코드가 실행 되기 전에 return 안 코드가 실행됨
+  // useEffect(()=>{ 
+  //   return ()=>{
+  //     실행할코드
+  //   }
+  // }, []) // 컴포넌트가 사라질 때 1회 실행됨
+  // useEffect(()=>{ 
+  //   실행할코드
+  // }, [state1]) // state1이 변경 될 때만 실행됨
+
 
   return (
   <>
-    <YellowBtn>버튼</YellowBtn>
-    {/* <YellowBtn bg='orange'>버튼</YellowBtn> */}
+    {/* <button onClick={ ()=>{setCount(count+1)} }>{count}카운트</button> */}
     <div className="container">
+      {
+        alert ? <div className="alert alert-warnig">2초 이내 구매시 할인</div> : null
+      }
       <div className="row">
         <div className="col-md-6">
           <img
